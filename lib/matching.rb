b7a39b0ef3f7071461c603f1f135c8f372e17d76
wumz <wumz@kexinbao100.com>
2017-09-11 22:18:27 +0800

    Config way change

--- lib/matching.rb
+++ lib/matching.rb
@@ -12,3 +12,19 @@ rescue NameError
     end
   end
 end
+
+module Matching
+  class << self
+    attr_accessor :order_traded
+    attr_accessor :order_canceled
+    attr_accessor :order_book_changed
+
+    def init!
+      @order_traded = -> data { puts data }
+      @order_canceled = -> data { puts data }
+      @order_book_changed = -> data { puts data }
+    end
+  end
+
+  init!
+end
